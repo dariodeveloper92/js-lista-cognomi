35e3f29627b1286906b0b56fa39af05488e94c26
Dario <dario.defrancisci92@gmail.com>
2021-07-20 15:55:55 +0200

    Caricato posizione umana

--- js/main.js
+++ js/main.js
@@ -1,19 +1,31 @@
 //CONSEGNA:
-//1. chiedi all’utente il cognome
+//1. chiedi all’utente il cognome--------------------------------------------------------------------
 var cognome = prompt('Inserisci il tuo cognome');
 
-//2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
-var lista_cognomi = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli, ' + cognome];
+//2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’--------------------------------------------------------------------
+var lista_cognomi = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli, ' + cognome + ' '];
 
 //Stampo lista Disordinata
 console.log(lista_cognomi);
 
 document.getElementById("cognomi_disordinati").innerHTML = lista_cognomi;
 
-//3. stampa la lista ordinata alfabeticamente
+//3. stampa la lista ordinata alfabeticamente--------------------------------------------------------------------
 //Stampo lista Ordinata
 console.log(lista_cognomi.sort());
 
 document.getElementById("cognomi_ordinati").innerHTML = lista_cognomi.sort();
 
-//4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+//4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova--------------------------------------------------------------------
+//INDEXOF
+console.log(lista_cognomi.indexOf('Bianchi') + ' Bianchi');
+console.log(lista_cognomi.indexOf('Neri') + ' Neri');
+console.log(lista_cognomi.indexOf('Rossi') + ' Rossi');
+console.log(lista_cognomi.indexOf('Verdi') + ' Verdi');
+console.log(lista_cognomi.indexOf('Gialli') + ' Gialli');
+console.log(lista_cognomi.indexOf('' + cognome) + ' '  + cognome);
+
+//SLICE
+var posizioneUmana = lista_cognomi.slice(0, 3,);
+
+document.getElementById("posizione_umana").innerHTML = posizioneUmana;
